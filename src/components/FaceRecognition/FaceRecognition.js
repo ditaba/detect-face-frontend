c6122da8c9f39af3ab046aa9733790006b0f51f2
xditaba <xditaba@gmail.com>
2019-05-03 22:11:23 +0700

    Update

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+const FaceRecognition = ({ imageUrl, box }) => {
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+        {box.map((value,index)=>{
+          return <div key={index} className='bounding-box' style={{top: value.topRow, right: value.rightCol, bottom: value.bottomRow, left: value.leftCol}}></div>
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default FaceRecognition;
